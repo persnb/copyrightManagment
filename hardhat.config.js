a4267171ca041218ce574ab777715b54fd680cf2
persnb <151069123+persnb@users.noreply.github.com>
2023-11-19 00:26:36 +0300

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,69 @@
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require("solidity-docgen");
+require("dotenv").config()
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-etherscan")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL 
+    const EscrollSEPOLIA_RPC_URL =
+    process.env.EscrollSEPOLIA_RPC_URL ||
+    "https://sepolia-rpc.scroll.io/"
+    const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+          chainId: 1337,
+          // gasPrice: 130000000000,
+      },
+      sepolia: {
+          url: SEPOLIA_RPC_URL,
+          accounts: [PRIVATE_KEY],
+          chainId: 11155111,
+          blockConfirmations: 6,
+      },
+      Escrollsepolia: {
+        url: SEPOLIA_RPC_URL,
+        accounts: [PRIVATE_KEY],
+        
+    }
+  },
+  solidity: {
+    version:"0.8.20",
+    settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }},
+  docgen:{},
+  etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+      customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
+  gasReporter: {
+      enabled: true,
+      currency: "USD",
+      outputFile: "gas-report.txt",
+      noColors: true,
+      // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+      deployer: {
+          default: 0, // here this will by default take the first account as deployer
+          1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      },
+  },
+  mocha: {
+      timeout: 500000,
+  },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,12 @@
+const networkConfig = {
+    1337: {
+        name: "localhost",
+    }
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
